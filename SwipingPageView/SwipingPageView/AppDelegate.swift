5bcbcad405cc24c020b257a215300686142e57b2
kwanghyun.won <wonkwh@gmail.com>
2019-02-11 00:28:45 +0900

    create project

---
+++ SwipingPageView/SwipingPageView/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwipingPageView
+//
+//  Created by wonkwh on 11/02/2019.
+//  Copyright © 2019 wonkwh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
